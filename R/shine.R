46e43feead6db50942375d674ce900b28b3dff5c
David Nusinow <david@david-pc>
2015-03-30 18:10:49 -0400

    Add initial shiny website implementation

---
+++ R/shine.R
@@ -0,0 +1,64 @@
+#' @export
+shine <- function() {
+    require("shiny")
+    require("cRomppass")
+
+    shinyApp(
+        
+        ui = fluidPage(
+            titlePanel("CompPASS"),
+            sidebarLayout(
+                sidebarPanel(
+                    fileInput("experimentUploader", "Experiments"),
+                    fileInput("statsUploader", "External Stats"),
+                    textInput("normFactor", "Normalization Factor", value = "0.98")
+                ),
+                mainPanel(downloadButton("download.scores", "Download"),
+                          dataTableOutput("scores"))
+            )
+        ),
+        
+        server = function(input, output) {
+
+            input.experiment <- reactive({
+                indf <- input$experimentUploader
+                if(is.null(indf)) {
+                    return(NULL)
+                }
+
+                read.delim(indf[1, "datapath"], stringsAsFactors = FALSE)
+            })
+
+            input.stats <- reactive({
+                indf <- input$statsUploader
+                if(is.null(indf)) {
+                    return(NULL)
+                }
+
+                read.delim(indf[1, "datapath"], stringsAsFactors = FALSE)
+            })
+                        
+            current.scores <- reactive({
+                experiment <- input.experiment()
+                stats <- input.stats()
+                if(is.null(experiment)) {
+                    return(NULL)
+                }
+                comppass(experiment, stats, norm.factor = as.numeric(input$normFactor))
+            })
+            
+            
+            output$scores <- renderDataTable({
+                current.scores()
+            })
+
+            output$download.scores <- downloadHandler(
+                filename = function() { "comppass_scores.tsv" },
+                content = function(file) {
+                    write.table(current.scores(), file,
+                                sep = "\t", row.names = FALSE)
+                }
+            )
+        }
+    )
+}
